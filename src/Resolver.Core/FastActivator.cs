5ce7dc5bd79dec448f6346691c8c7fb51751ea24
Satish Prajapati <sprajapati@tavisca.com>
2017-09-01 14:33:32 +0530

    Initial commit

---
+++ src/Resolver.Core/FastActivator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resolver.Core
+{
+    public static class FastActivator
+    {
+        // THIS VERSION NOT THREAD SAFE YET
+        static Dictionary<Type, Func<object>> constructorCache = new Dictionary<Type, Func<object>>();
+
+        private const string DynamicMethodPrefix = "DM$_FastActivator_";
+
+        public static object CreateInstance(Type objType)
+        {
+            return GetConstructor(objType)();
+        }
+
+        private static Func<object> GetConstructor(Type objType)
+        {
+            Func<object> constructor;
+            if (!constructorCache.TryGetValue(objType, out constructor))
+            {
+                constructor = (Func<object>)FastActivator.BuildConstructorDelegate(objType, typeof(Func<object>), new Type[] { });
+                constructorCache.Add(objType, constructor);
+            }
+            return constructor;
+        }
+
+        public static object BuildConstructorDelegate(Type objType, Type delegateType, Type[] argTypes)
+        {
+            var dynMethod = new DynamicMethod(DynamicMethodPrefix + objType.Name + "$" + argTypes.Length.ToString(), objType, argTypes, objType);
+            ILGenerator ilGen = dynMethod.GetILGenerator();
+            for (int argIdx = 0; argIdx < argTypes.Length; argIdx++)
+            {
+                ilGen.Emit(OpCodes.Ldarg, argIdx);
+            }
+            ilGen.Emit(OpCodes.Newobj, objType.GetConstructor(argTypes));
+            ilGen.Emit(OpCodes.Ret);
+            return dynMethod.CreateDelegate(delegateType);
+        }
+    }
+}
---
+++ src/Resolver.Core/ObjectBuilder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resolver.Core
+{
+    public class ObjectBuilder
+    {
+        private static Dictionary<TypeMap, Type> mapper = new Dictionary<TypeMap, Type>();
+
+        public static T Resolve<T>(string mapName = "")
+        {
+            var map = new TypeMapper(typeof(T), mapName);
+            if (mapper.ContainsKey(map))
+            {
+                return (T)Activate(mapper[map]);
+            }
+            return default(T);
+        }
+        private static object Activate(Type type)
+        {
+            return FastActivator.CreateInstance(type);
+        }
+        internal static void Register<Tp, Tc>(string name)
+        {
+            var map = new TypeMap(typeof(Tp), name);
+            mapper[map] = typeof(Tc);
+        }
+    }
+    public class Register
+    {
+        protected Type RgisteredType;
+        protected RegisterExpression<T> For<T>()
+        {
+            return new RegisterExpression<T>();
+        }
+        public class RegisterExpression<Tp>
+        {
+            public void Use<Tc>(string name = "") where Tc : Tp
+            {
+                ObjectBuilder.Register<Tp, Tc>(name);
+            }
+        }
+    }
+    
+    internal class TypeMap
+    {
+        private string _name;
+        private string _mapName;
+        private Type _type;
+        public TypeMap(Type type, string map = "")
+        {
+            _type = type;
+            _name = _type.FullName;
+            _mapName = map;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj != null && obj is TypeMap && GetHashCode() == obj.GetHashCode();
+        }
+        public override int GetHashCode()
+        {
+            return _name.GetHashCode() ^ _mapName.GetHashCode();
+        }
+    }
+
+    internal class Resolver
+    {
+        public static object GetConstructor(Type type)
+        {
+            ConstructorInfo[] info = type.GetConstructors();
+
+            if (info != null && info.Length > 0)
+            {
+                var parameters = info[0].GetParameters();
+            }
+
+        }
+    }
+}
